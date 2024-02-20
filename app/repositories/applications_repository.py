d6307ad5447c470e84ff81148c1758f574d22caa
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-20 21:07:33 +0100

    Add documentation for repository functions.

--- app/repositories/applications_repository.py
+++ app/repositories/applications_repository.py
@@ -8,6 +8,18 @@ from app.models.person import Person
 
 
 def get_application_statuses_from_db() -> list[ApplicationStatus]:
+    """
+    Retrieves application statuses from the database.
+
+    This function fetches all application statuses from the database. It
+    raises an exception if there is a database issue or if no application
+    statuses are found.
+
+    :returns: A list of ApplicationStatus objects.
+    :raises SQLAlchemyError: If there is a database issue.
+    :raises NoResultFound: If no application statuses are found.
+    """
+
     try:
         application_statuses = ApplicationStatus.query.all()
     except SQLAlchemyError as exception:
@@ -21,6 +33,19 @@ def get_application_statuses_from_db() -> list[ApplicationStatus]:
 
 
 def get_personal_info_from_db(person_id: int) -> Person:
+    """
+    Retrieves personal information of a user from the database.
+
+    This function fetches the personal information of a user with the given
+    person_id from the database. It raises an exception if there is a database
+    issue or if no user is found.
+
+    :param person_id: The id of the user to retrieve information for.
+    :returns: A Person object representing the user.
+    :raises SQLAlchemyError: If there is a database issue.
+    :raises NoResultFound: If no user is found.
+    """
+
     try:
         applicant = Person.query.filter_by(person_id=person_id).first()
     except SQLAlchemyError as exception:
@@ -34,6 +59,18 @@ def get_personal_info_from_db(person_id: int) -> Person:
 
 
 def get_competences_from_db(person_id: int) -> list[CompetenceProfile]:
+    """
+    Retrieves competences of a user from the database.
+
+    This function fetches the competences of a user with the given person_id
+    from the database. It raises an exception if there is a database issue.
+
+    :param person_id: The id of the user to retrieve competences for.
+    :returns: A list of CompetenceProfile objects representing the user's
+              competences.
+    :raises SQLAlchemyError: If there is a database issue.
+    """
+
     try:
         return CompetenceProfile.query.filter_by(person_id=person_id).all()
     except SQLAlchemyError as exception:
@@ -42,6 +79,20 @@ def get_competences_from_db(person_id: int) -> list[CompetenceProfile]:
 
 
 def get_availabilities_from_db(person_id: int) -> list[CompetenceProfile]:
+    """
+    Retrieves availabilities of a user from the database.
+
+    This function fetches the availabilities of a user with the given person_id
+    from the database. It raises an exception if there is a database issue or
+    if no availabilities are found.
+
+    :param person_id: The id of the user to retrieve availabilities for.
+    :returns: A list of Availability objects representing the user's
+              availabilities.
+    :raises SQLAlchemyError: If there is a database issue.
+    :raises NoResultFound: If no availabilities are found.
+    """
+
     try:
         availabilities = Availability.query.filter_by(
                 person_id=person_id).all()
