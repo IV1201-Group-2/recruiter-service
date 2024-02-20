4909fe328e832e856c24618d1e04465d116b4b3e
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-20 19:00:17 +0100

    Implement endpoint to retrieve applications.

---
+++ app/routes/applications_route.py
@@ -0,0 +1,54 @@
+from flask import Blueprint, Response, current_app, jsonify
+from flask_jwt_extended import get_jwt, jwt_required
+from sqlalchemy.exc import NoResultFound, SQLAlchemyError
+
+from app.services.applications_service import compile_applications
+from app.utilities.status_codes import StatusCodes
+
+applications_bp = Blueprint('applications', __name__)
+
+
+@applications_bp.route('/', methods=['GET'])
+@jwt_required()
+def get_applications() -> tuple[Response, int]:
+    if get_jwt()['role'] != 1:
+        return jsonify({'error': 'UNAUTHORIZED'}), StatusCodes.UNAUTHORIZED
+
+    try:
+        errors, applications = compile_applications()
+
+        if not errors:
+            current_app.logger.info('Responding with applications.')
+            return jsonify(applications), StatusCodes.OK
+        elif 0 < len(errors) < len(applications):
+            current_app.logger.info('Responding with applications and errors.')
+            return (jsonify({'applications': applications, 'errors': errors}),
+                    StatusCodes.PARTIAL_CONTENT)
+        else:
+            current_app.logger.error('Could not fetch applications.')
+            return (jsonify({'error': 'COULD_NOT_FETCH_APPLICATIONS'}),
+                    StatusCodes.INTERNAL_SERVER_ERROR)
+
+    except NoResultFound as exception:
+        return (jsonify({'error': exception.args[0]}),
+                StatusCodes.NOT_FOUND)
+    except SQLAlchemyError:
+        return (jsonify({'error': 'COULD_NOT_FETCH_APPLICATIONS'}),
+                StatusCodes.INTERNAL_SERVER_ERROR)
+
+
+"""
+@applications_bp.route('/<int:person_id>/<str:status>', methods=['POST'])
+@jwt_required()
+def get_applicationss() -> tuple[Response, int]:
+    if get_jwt()['role'] != 1:
+        return jsonify({'error': 'UNAUTHORIZED'}), StatusCodes.UNAUTHORIZED
+
+    try:
+        applications = compile_applications()
+        current_app.logger.info('Responding with applications.')
+        return jsonify(applications), StatusCodes.OK
+    except SQLAlchemyError:
+        return (jsonify({'error': 'COULD_NOT_FETCH_APPLICATIONS'}),
+                StatusCodes.INTERNAL_SERVER_ERROR)
+"""
