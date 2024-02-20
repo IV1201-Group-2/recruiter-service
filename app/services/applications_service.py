9339e8293922ba2d383b1d8762825b0c538addd7
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-20 19:00:41 +0100

    Implement application compilation logic.

---
+++ app/services/applications_service.py
@@ -0,0 +1,35 @@
+from sqlalchemy.exc import NoResultFound, SQLAlchemyError
+
+from app.repositories.applications_repository import \
+    get_application_statuses_from_db, get_availabilities_from_db, \
+    get_competences_from_db, get_personal_info_from_db
+
+
+def compile_applications() -> tuple[list, list]:
+    application_statuses = get_application_statuses_from_db()
+    compiled_applications = []
+    errors = []
+
+    for entry in application_statuses:
+        try:
+            person_id = entry.person_id
+            personal_info = get_personal_info_from_db(person_id).to_dict()
+            competences = get_competences_from_db(person_id)
+            availabilities = get_availabilities_from_db(person_id)
+
+            compiled_application = {
+                'personal_info': personal_info,
+                'competences': [competence.to_dict() for competence in
+                                competences],
+                'availabilities': [availability.to_dict() for availability in
+                                   availabilities],
+                'status': entry.status
+            }
+
+            compiled_applications.append(compiled_application)
+
+        except (NoResultFound, SQLAlchemyError) as exception:
+            errors.append({'error': exception.args[0]})
+            continue
+
+    return errors, compiled_applications
