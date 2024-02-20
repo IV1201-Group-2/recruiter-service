e4d454ab3a110bd550d0ae7c2077173be14636b7
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-20 19:08:14 +0100

    Add unit tests for retrieving applications.

---
+++ tests/routes/test_applications_route.py
@@ -0,0 +1,139 @@
+from unittest.mock import patch
+
+from sqlalchemy.exc import SQLAlchemyError
+
+from tests.utilities.status_codes import StatusCodes
+from tests.utilities.utility_functions import assert_application_details, \
+    cleanup_db, generate_token_for_person_id_1, generate_token_for_recruiter, \
+    post_request_applications_endpoint, remove_application_statuses_from_db, \
+    remove_availabilities_from_db, remove_competence_profiles_from_db, \
+    remove_users_from_db, setup_application_statuses_for_all_users, \
+    setup_availabilities_for_all_users, setup_availability_for_user2_in_db, \
+    setup_availability_for_user3_in_db, \
+    setup_competence_profiles_for_all_users, setup_three_users
+
+
+def test_get_applications_success(app_with_client):
+    app, test_client = app_with_client
+    setup_three_users(app)
+    setup_application_statuses_for_all_users(app)
+    setup_availabilities_for_all_users(app)
+    setup_competence_profiles_for_all_users(app)
+
+    token = generate_token_for_recruiter(app)
+    response = post_request_applications_endpoint(test_client, token)
+
+    assert response.status_code == StatusCodes.OK
+
+    applications = response.json
+    assert len(applications) == 3
+
+    assert_application_details(
+            applications[0], 'user1', 's1', '1', 'UNHANDLED',
+            [{'competence_id': 1, 'years_of_experience': '1.00'},
+             {'competence_id': 1, 'years_of_experience': '4.00'}],
+            [{'from_date': '2024-03-01', 'to_date': '2024-03-02'},
+             {'from_date': '2024-03-04', 'to_date': '2024-03-05'}])
+
+    assert_application_details(
+            applications[1], 'user2', 's2', '2', 'UNHANDLED',
+            [{'competence_id': 2, 'years_of_experience': '2.00'}],
+            [{'from_date': '2024-03-02', 'to_date': '2024-03-03'}])
+
+    assert_application_details(
+            applications[2], 'user3', 's3', '3', 'UNHANDLED',
+            [],
+            [{'from_date': '2024-03-03', 'to_date': '2024-03-04'}])
+
+    remove_users_from_db(app)
+    remove_competence_profiles_from_db(app)
+    remove_availabilities_from_db(app)
+    remove_application_statuses_from_db(app)
+
+
+def test_get_applications_partial_success(app_with_client):
+    app, test_client = app_with_client
+    setup_three_users(app)
+    setup_application_statuses_for_all_users(app)
+    setup_availability_for_user2_in_db(app)
+    setup_availability_for_user3_in_db(app)
+    setup_competence_profiles_for_all_users(app)
+
+    token = generate_token_for_recruiter(app)
+    response = post_request_applications_endpoint(test_client, token)
+
+    assert response.status_code == StatusCodes.PARTIAL_CONTENT
+
+    applications = response.json
+    assert len(applications) == 2
+
+    successfully_fetched = applications['applications']
+    assert len(successfully_fetched) == 2
+
+    assert_application_details(
+            successfully_fetched[0], 'user2', 's2', '2', 'UNHANDLED',
+            [{'competence_id': 2, 'years_of_experience': '2.00'}],
+            [{'from_date': '2024-03-02', 'to_date': '2024-03-03'}])
+
+    assert_application_details(
+            successfully_fetched[1], 'user3', 's3', '3', 'UNHANDLED',
+            [],
+            [{'from_date': '2024-03-03', 'to_date': '2024-03-04'}])
+
+    failed_to_fetch = applications['errors']
+    assert len(failed_to_fetch) == 1
+    assert failed_to_fetch[0][
+               'error'] == 'NO_AVAILABILITIES_FOUND_FOR_PERSON: 1'
+
+    cleanup_db(app)
+
+
+def test_get_applications_not_found(app_with_client):
+    app, test_client = app_with_client
+    setup_three_users(app)
+    setup_availabilities_for_all_users(app)
+    setup_competence_profiles_for_all_users(app)
+
+    token = generate_token_for_recruiter(app)
+    response = post_request_applications_endpoint(test_client, token)
+
+    assert response.status_code == StatusCodes.NOT_FOUND
+    assert response.json['error'] == 'NO_APPLICATION_STATUSES_FOUND'
+
+    cleanup_db(app)
+
+
+def test_get_applications_only_errors(app_with_client):
+    app, test_client = app_with_client
+    setup_three_users(app)
+    setup_competence_profiles_for_all_users(app)
+    setup_application_statuses_for_all_users(app)
+
+    token = generate_token_for_recruiter(app)
+    response = post_request_applications_endpoint(test_client, token)
+
+    assert response.status_code == StatusCodes.INTERNAL_SERVER_ERROR
+    assert response.json['error'] == 'COULD_NOT_FETCH_APPLICATIONS'
+
+    cleanup_db(app)
+
+
+def test_get_applications_unauthorized(app_with_client):
+    app, test_client = app_with_client
+    token = generate_token_for_person_id_1(app)
+
+    response = post_request_applications_endpoint(test_client, token)
+    assert response.status_code == StatusCodes.UNAUTHORIZED
+
+
+@patch('app.routes.applications_route.compile_applications')
+def test_get_applications_sqlalchemy_error(mock_fetch, app_with_client):
+    app, test_client = app_with_client
+    token = generate_token_for_recruiter(app)
+
+    mock_fetch.side_effect = SQLAlchemyError
+
+    response = post_request_applications_endpoint(test_client, token)
+
+    assert response.status_code == StatusCodes.INTERNAL_SERVER_ERROR
+    assert response.json['error'] == 'COULD_NOT_FETCH_APPLICATIONS'
