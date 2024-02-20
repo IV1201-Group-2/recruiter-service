82621231947457fd2e7afd5043ab71e5949d1b21
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-20 22:57:34 +0100

    Remove redundant cleanup. Reorganise setup to follow DB constraints.

--- tests/services/test_applications_service.py
+++ tests/services/test_applications_service.py
@@ -4,42 +4,39 @@ import pytest
 from sqlalchemy.exc import NoResultFound, SQLAlchemyError
 
 from app.services.applications_service import compile_applications
-from tests.utilities.utility_functions import cleanup_db, \
+from tests.utilities.utility_functions import \
     setup_application_statuses_for_all_users, \
     setup_availabilities_for_all_users, \
-    setup_competence_profiles_for_all_users, setup_three_users, \
-    setup_user1_in_db, setup_user2_in_db
+    setup_availability_for_user2_in_db, \
+    setup_availability_for_user3_in_db, \
+    setup_competence_profiles_for_all_users, setup_three_users
 
 
 def test_compile_applications_only_success(app_with_client):
     app, _ = app_with_client
+    setup_three_users(app)
     setup_application_statuses_for_all_users(app)
     setup_availabilities_for_all_users(app)
     setup_competence_profiles_for_all_users(app)
-    setup_three_users(app)
 
     with app.app_context():
         errors, applications = compile_applications()
         assert len(errors) == 0
         assert len(applications) == 3
 
-    cleanup_db(app)
-
 
 def test_compile_applications_partial_success(app_with_client):
     app, _ = app_with_client
-    setup_user1_in_db(app)
-    setup_user2_in_db(app)
+    setup_three_users(app)
     setup_application_statuses_for_all_users(app)
-    setup_availabilities_for_all_users(app)
+    setup_availability_for_user2_in_db(app)
+    setup_availability_for_user3_in_db(app)
     setup_competence_profiles_for_all_users(app)
 
     with app.app_context():
         errors, applications = compile_applications()
         assert len(errors) == 1
         assert len(applications) == 2
-
-    cleanup_db(app)
 
 
 def test_compile_applications_only_errors(app_with_client):
@@ -52,8 +49,6 @@ def test_compile_applications_only_errors(app_with_client):
         errors, applications = compile_applications()
         assert len(errors) == 3
         assert len(applications) == 0
-
-    cleanup_db(app)
 
 
 @patch('app.services.applications_service.get_application_statuses_from_db')
