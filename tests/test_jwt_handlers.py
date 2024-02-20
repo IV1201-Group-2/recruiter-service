b7b334da36c44e2a32e3dc3495c09ccfc3d442bd
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-20 22:58:21 +0100

    Remove redundant cleanup.

--- tests/test_jwt_handlers.py
+++ tests/test_jwt_handlers.py
@@ -5,7 +5,6 @@ from flask_jwt_extended import create_access_token
 from tests.utilities.status_codes import StatusCodes
 from tests.utilities.utility_functions import generate_token_for_person_id_1, \
     generate_token_for_recruiter, post_request_applications_endpoint, \
-    remove_users_from_db, \
     setup_application_status_for_user1_in_db, \
     setup_availabilities_for_user1_in_db, \
     setup_user1_in_db
@@ -21,7 +20,6 @@ def test_valid_token(app_with_client):
 
     response = post_request_applications_endpoint(test_client, valid_token)
     assert response.status_code == StatusCodes.OK
-    remove_users_from_db(app)
 
 
 def test_invalid_token(app_with_client):
@@ -50,7 +48,6 @@ def test_expired_token(app_with_client):
                                                   expired_token)
     assert response.status_code == StatusCodes.UNAUTHORIZED
     assert response.json['error'] == 'TOKEN_EXPIRED'
-    remove_users_from_db(app)
 
 
 def test_unauthorized_request(app_with_client):
