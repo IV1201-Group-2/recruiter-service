dafff9c5318855d5a4c3e7f16c691e65db2cc19d
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-29 18:18:27 +0100

    Add specific handling of incorrect URL and favicon requests.

--- app/routes/error_handler.py
+++ app/routes/error_handler.py
@@ -1,4 +1,7 @@
-from flask import current_app, jsonify
+import logging
+
+from flask import jsonify, request
+from werkzeug.exceptions import HTTPException
 
 from app.utilities.status_codes import StatusCodes
 
@@ -8,13 +11,23 @@ def handle_all_unhandled_exceptions(exception):
     Handle all unhandled exceptions.
 
     This function handles all unhandled exceptions by logging the exception and
-    returning a JSON response with an 'INTERNAL_SERVER_ERROR' message and a 500
-    status code.
+    returning an appropriate JSON response with a status code.
 
     :param exception: The unhandled exception.
     :returns: A tuple containing a JSON response and a status code.
     """
 
-    current_app.logger.error(exception)
+    requester_ip = request.remote_addr
+
+    if isinstance(exception, HTTPException) and 'favicon.ico' in request.url:
+        logging.warning(f'{requester_ip} - Favicon not found')
+        return jsonify({'error': 'NOT_FOUND'}), StatusCodes.NOT_FOUND
+
+    if (isinstance(exception, HTTPException) and
+            exception.code == StatusCodes.NOT_FOUND):
+        logging.error(f'{requester_ip} - URL not found: {request.url}')
+        return jsonify({'error': 'NOT_FOUND'}), StatusCodes.NOT_FOUND
+
+    logging.critical(f'{requester_ip} - ' + str(exception))
     return (jsonify({'error': 'INTERNAL_SERVER_ERROR'}),
             StatusCodes.INTERNAL_SERVER_ERROR)
