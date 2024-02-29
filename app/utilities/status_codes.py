9e340514b668dcd3c662dda1e0861ef253f5fba1
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-29 18:16:24 +0100

    Add missing documentation.

--- app/utilities/status_codes.py
+++ app/utilities/status_codes.py
@@ -1,4 +1,14 @@
 class StatusCodes:
+    """
+    Represents the HTTP status codes that can be returned by the API.
+
+    :ivar OK: The request was successful.
+    :ivar PARTIAL_CONTENT: The request was partially successful.
+    :ivar UNAUTHORIZED: The request was unauthorized.
+    :ivar NOT_FOUND: The resource was not found.
+    :ivar INTERNAL_SERVER_ERROR: An internal server error occurred.
+    """
+
     OK = 200
     PARTIAL_CONTENT = 206
     UNAUTHORIZED = 401
