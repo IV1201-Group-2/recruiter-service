f5929ed00ff7a8eb39d89ac2a326cae5994cbae3
Azmeer K. <azmeer.khawaja@outlook.com>
2024-03-04 18:02:09 +0100

    Add usage of Postgres test-container.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,25 @@
+import os
+
 import pytest
+from testcontainers.postgres import PostgresContainer  # type: ignore
 
 from app.app import create_app, database
 from tests.utilities.utility_functions import cleanup_db
 
 
+@pytest.fixture(scope='session')
+def postgres():
+    with PostgresContainer('postgres:latest') as postgres:
+        yield postgres
+
+
 @pytest.fixture(scope='function')
-def app_with_client():
+def app_with_client(postgres):
+    os.environ['DATABASE_URL'] = postgres.get_connection_url()
     flask_app = create_app()
     flask_app.config.update({
         'TESTING': True,
         'JWT_SECRET_KEY': 'your-test-secret-key',
-        'SQLALCHEMY_DATABASE_URI': 'sqlite:///:memory:',
     })
 
     with flask_app.app_context():
